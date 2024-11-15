9a4977bc1f402bcd48261de6d87e8ad971fcb95f
neruiz3 <nerea.ruiz@usach.cl>
2024-11-15 19:57:21 -0300

    simulacionCredito-service
    Se ha creado el primer microservicio, solo para las simulaciones de crédito

---
+++ Backend-Microservices/simulacionCredito-service/src/main/java/com/example/credito_service/entity/SimulacionCreditoEntity.java
@@ -0,0 +1,43 @@
+package com.example.credito_service.entity;
+
+import com.example.credito_service.Estado;
+import com.example.credito_service.TipoPrestamo;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "simulaciones")
+@Data //genera automaticamente los getters y setters
+@NoArgsConstructor //genera un constructor sin argumentos para la clase
+@AllArgsConstructor //constructor con todos los argumentos de la clase, por si se los pasas
+public class SimulacionCreditoEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(unique = true, nullable = false)
+    private Long id;
+
+    private String rut; //para saber el cliente que ha solicitado ese credito, no se si hace fala realmente
+
+    @Column(name = "plazo", nullable = false)
+    private int plazo;
+
+    @Column(name = "tasaInteres", nullable = false)
+    private double tasaInteres;
+
+    @Column(name = "monto", nullable = false)
+    private double monto;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "tipoPrestamo", nullable = false)
+    private TipoPrestamo tipoPrestamo;
+
+    private double valorPropiedad;
+
+    private double cuotaMensual;
+
+    @Enumerated(EnumType.STRING)
+    private Estado estado = Estado.EN_REVISION_INICIAL; //por defecto se inicializa este valor
+}
