3e265d4d94991e8c26c98644ad73ee201fa86914
neruiz3 <nerea.ruiz@usach.cl>
2024-11-15 19:00:40 -0300

    Add files via upload

---
+++ Evaluacion_2/Backend-Microservices/credito-service/src/main/java/com/example/credito_service/CreditoServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.credito_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditoServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreditoServiceApplication.class, args);
+	}
+
+}
