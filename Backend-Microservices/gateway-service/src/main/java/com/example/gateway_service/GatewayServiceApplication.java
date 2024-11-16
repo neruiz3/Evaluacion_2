9632d452b24126ea5d9a9afacc0b6f48dfd28a39
neruiz3 <nerea.ruiz@usach.cl>
2024-11-16 13:08:44 -0300

    gateway-service
    se ha creado el gateway-service y ahi se iran añadiendo los distintos miccroservicios

--- Backend-Microservices/gateway-service/src/main/java/com/example/gateway_service/GatewayServiceApplication.java
+++ Backend-Microservices/gateway-service/src/main/java/com/example/gateway_service/GatewayServiceApplication.java
@@ -1,13 +1,13 @@
-package com.example.credito_service;
+package com.example.gateway_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SimulacionCreditoServiceApplication {
+public class GatewayServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SimulacionCreditoServiceApplication.class, args);
+		SpringApplication.run(GatewayServiceApplication.class, args);
 	}
 
 }
