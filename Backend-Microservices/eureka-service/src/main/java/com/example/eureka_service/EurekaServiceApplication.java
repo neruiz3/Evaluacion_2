b475df35fa73ffcb0f999d9cd69dac054b87ca26
neruiz3 <nerea.ruiz@usach.cl>
2024-11-16 12:18:25 -0300

    Eureka-service
    Se ha configurado eureka

--- Backend-Microservices/eureka-service/src/main/java/com/example/eureka_service/EurekaServiceApplication.java
+++ Backend-Microservices/eureka-service/src/main/java/com/example/eureka_service/EurekaServiceApplication.java
@@ -1,9 +1,12 @@
 package com.example.eureka_service;
 
+import jdk.jfr.Enabled;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class EurekaServiceApplication {
 
 	public static void main(String[] args) {
